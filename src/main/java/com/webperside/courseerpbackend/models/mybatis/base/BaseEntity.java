5ad94bffc094eb409621cd77f6174b8660853313
farhadnajafov <farhad.najafov@e-manat.az>
2023-10-02 13:54:02 +0400

    changes

--- src/main/java/com/webperside/courseerpbackend/models/mybatis/base/BaseEntity.java
+++ src/main/java/com/webperside/courseerpbackend/models/mybatis/base/BaseEntity.java
@@ -1,15 +1,17 @@
 package com.webperside.courseerpbackend.models.mybatis.base;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 
 @Data
+@SuperBuilder(builderMethodName = "baseBuilder")
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class BaseEntity <T> extends IsDeletedEntity {
+public class BaseEntity<ID> extends IsDeletedEntity {
 
-    T id;
+    ID id;
 
 }
